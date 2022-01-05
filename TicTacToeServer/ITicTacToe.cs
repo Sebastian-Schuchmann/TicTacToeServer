ad8c5d4c2eded9753514b835500b041bce3d7892
Seppi <seppi@ditached.de>
2022-01-05 09:01:13 +0100

    Documented Interface

--- TicTacToeServer/ITicTacToe.cs
+++ TicTacToeServer/ITicTacToe.cs
@@ -11,7 +11,20 @@ public enum GameState
 public interface ITicTacToe
 {
     public void StartGame();
+    
+    /// <summary>
+    /// Executes a move on the board after a game has been started.
+    /// </summary>
+    /// <param name="playerId">PlayerID is either 0 (PlayerX) or 1 (PlayerO)</param>
+    /// <param name="selectedField">SelectedField is a value between 0 and 8 where 0 represents the top left field</param>
+    /// <returns>Returns the GameState Enum (InProgress / Draw / PlayerXWon / PlayerOWon)</returns>
     public GameState MakeTurn(int playerId, int selectedField);
+    
+    /// <summary>
+    /// Return the TicTacToe board as an array of integers.
+    /// </summary>
+    /// <returns>Elements in the array can have the following values: -1, 0, 1, where -1 means Empty and 0 / 1 represent the first/second player</returns>
     public int[] GetField();
+    
     public string GetFieldPretty();
 }
